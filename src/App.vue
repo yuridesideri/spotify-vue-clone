7e8992b56f9ece7c2f31498a52a69d4ad6da5a8d
Yuri Desideri <yuridesideri12@gmail.com>
2023-05-09 22:02:43 -0300

    project setup

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <nav class="sticky left-0 top-0 h-screen w-48">
+    <RouterLink to="/">
+      <img src="@/components/icons/Spotify_Logo_RGB_White.png" class="w-32" />
+    </RouterLink>
+
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+  <main></main>
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      white: '#ffffff',
+      spblack: '#191414',
+      spwhite: '#ffffff',
+      spgreen: '#1db954',
+      spgray: '#535353'
+    },
+    extend: {}
+  },
+  plugins: []
+}
