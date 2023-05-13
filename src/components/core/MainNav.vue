4016e3e4530203e864ab455ab53d035713d62a1e
Yuri Desideri <yuridesideri12@gmail.com>
2023-05-12 23:06:34 -0300

    feat: hover styles to navbar

--- src/components/core/MainNav.vue
+++ src/components/core/MainNav.vue
@@ -6,17 +6,21 @@ import { useRoute, type RouteRecordName } from 'vue-router'
 const route = useRoute()
 
 function checkActiveNav(routeName: RouteRecordName) {
-  return route.name === routeName ? 'font-bold' : 'font-normal'
+  return route.name === routeName ? '' : ''
 }
 </script>
 
 <template>
   <div class="flex h-full w-full flex-col justify-around">
-    <RouterLink to="/" id="home-link" class="flex">
+    <RouterLink to="/" id="home-link" class="flex gap-5 p-2 font-bold hover:text-[#fff]">
       <HomeIcon />
-      <h2 :class="[checkActiveNav('home')]">Home</h2>
+      <h2 :class="[checkActiveNav('home')]" class="text-[#cecece]">Home</h2>
     </RouterLink>
-    <RouterLink to="/search" id="search-link" class="flex">
+    <RouterLink
+      to="/search"
+      id="search-link"
+      class="flex gap-5 p-2 font-semibold hover:text-[#fff]"
+    >
       <SearchIcon />
       <h2 :class="[checkActiveNav('search')]">Search</h2>
     </RouterLink>
