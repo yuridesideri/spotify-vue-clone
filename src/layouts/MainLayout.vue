fa5558864b87a28d16d7f177611d5e216684ad02
Yuri Desideri <yuridesideri12@gmail.com>
2023-05-14 01:58:02 -0300

    fix: template cells

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -5,21 +5,21 @@ import MainNav from '@/components/core/MainNav.vue'
 </script>
 
 <template>
-  <div id="grid" class="flex h-full w-full flex-col">
-    <div id="main-layout" class="flex h-full flex-grow">
-      <div id="navigation" class="flex h-full w-80 flex-col p-2">
-        <nav id="main-navigation" class="h-20 bg-spcell 2-full mb-2 rounded-lg">
+  <div id="grid" class=" flex h-full w-full flex-col">
+    <div id="main-layout" class="flex h-full flex-grow p-2 gap-2">
+      <div id="navigation" class="flex h-full w-80 flex-col">
+        <nav id="main-navigation" class="2-full mb-2 h-20 rounded-lg bg-spcell">
           <MainNav />
         </nav>
-        <nav id="library-navigation" class="w-full flex-grow bg-spcell rounded-lg">
+        <nav id="library-navigation" class="w-full flex-grow rounded-lg bg-spcell">
           <LibraryNav />
         </nav>
       </div>
-      <main id="content" class="bg-red">
+      <main id="content" class="flex-grow rounded-lg bg-spcell">
         <MainContent />
       </main>
     </div>
-    <footer class="h-20 w-full bg-spcell"></footer>
+    <footer class="h-20 w-full"></footer>
   </div>
 </template>
 
