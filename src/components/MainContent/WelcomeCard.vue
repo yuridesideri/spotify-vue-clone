22621afa04f1831df171e036eeb57ece67491923
Yuri Desideri <yuridesideri12@gmail.com>
2023-06-05 23:44:49 -0300

    fix: rounded image border

--- src/components/MainContent/WelcomeCard.vue
+++ src/components/MainContent/WelcomeCard.vue
@@ -17,7 +17,7 @@ withDefaults(
         class="flex items-center gap-4 rounded-md bg-[#302F35] hover:bg-[#454447] hover:brightness-100"
     >
         <div class="relative flex aspect-square h-full">
-            <img :src="image" alt="" class="absolute aspect-square h-full w-full" />
+            <img :src="image" alt="" class="absolute aspect-square h-full w-full rounded-l-md" />
         </div>
         <div>
             <h1 class="text-white">{{ title }}</h1>
